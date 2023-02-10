635a906d3020be685fc07756768e06b237bb97df
sveta <strekaleksey@gmail.com>
2023-02-10 15:47:49 +0900

    10/02/23

--- HotelsPage.xaml.cs
+++ HotelsPage.xaml.cs
@@ -23,9 +23,18 @@ namespace ToursApp
         public HotelsPage()
         {
             InitializeComponent();
+            DGridHotels.ItemsSource = Николаев_РобертEntities.GetContext().Hotel.ToList();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage());
+        }
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage());
+        }
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
         {
             Manager.MainFrame.Navigate(new AddEditPage());
         }
